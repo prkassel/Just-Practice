6afe17fb719cd3b0b2d572a786e6add719e8259a
Philip Kassel <prkassel@gmail.com>
2019-12-27 12:59:30 -0600

    Initial Commit

---
+++ JustPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JustPractice
+//
+//  Created by Philip Kassel on 12/27/19.
+//  Copyright © 2019 Philip Kassel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
